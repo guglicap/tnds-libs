9cdda59164db496c0db009149165506a82da10aa
guglicap <guglielmocaprotti@gmail.com>
2021-11-26 11:21:58 +0100

    midpoint.h: added const to some variables, fixed unsigned nStep

--- include/math/integral/midpoint.h
+++ include/math/integral/midpoint.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#define INTEGRAL_MIDPOINT_DEFAULT_NSTEP 1000
 #include "math/integral/integral.h"
 
 namespace math {
@@ -9,12 +10,12 @@ namespace math {
         public:
             Midpoint(Func f) : Integral(), f(f) {};
 
-            Midpoint(Func f, int nStep) : Integral(), f(f), nStep(nStep) {};
+            Midpoint(Func f, unsigned int nStep) : Integral(), f(f), nStep(nStep) {};
 
             double Integrate(double a, double b) const override {
                 auto res = 0.0;
-                auto step = (b - a) / double(nStep);
-                for (int i = 0; i < nStep; i++) {
+                const auto step = (b - a) / double(nStep);
+                for (unsigned int i = 0; i < nStep; i++) {
                     res += integrate(a + step * i, a + step * (i + 1));
                 }
                 return res;
@@ -22,10 +23,10 @@ namespace math {
 
         private:
             const Func f;
-            const int nStep = 100;
+            const unsigned int nStep = INTEGRAL_MIDPOINT_DEFAULT_NSTEP;
 
             constexpr double integrate(double a, double b) const {
-                auto xM = (a + b) / 2;
+                const auto xM = (a + b) / 2;
                 return f(xM) * (b - a);
             }
         };
