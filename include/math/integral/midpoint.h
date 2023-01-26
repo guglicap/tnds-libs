52836e19e080f51fb2f81ab5d68819991582af46
guglicap <guglielmocaprotti@gmail.com>
2023-01-26 11:02:07 +0100

    math::integral::Midpoint fixed formula, added runtime error estimate

--- include/math/integral/midpoint.h
+++ include/math/integral/midpoint.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define INTEGRAL_MIDPOINT_DEFAULT_NSTEP 1000
+#define INTEGRAL_MIDPOINT_DEFAULT_NSTEP 2
 
 #include "math/integral/integral.h"
 
@@ -9,24 +9,57 @@ namespace math {
         template<typename Func>
         class Midpoint : public Integrator {
         public:
-            Midpoint(Func f) : Integrator(), f(f) {};
+            Midpoint(Func f) : Integrator(), f{f} {};
 
-            Midpoint(Func f, unsigned int N) : Integrator(), f(f), N(N) {};
+            Midpoint(Func f, unsigned int N) : Integrator(), f{f}, N{N} {};
+
+            Midpoint(Func f, double reqPrec) : Integrator(), f{f}, reqPrec{reqPrec} {};
 
             Result Integrate(double a, double b) override {
+                for (;;) {
+                    auto res = integrate(a, b);
+                    if (std::isnan(reqPrec)) {
+                        return res;
+                    }
+                    auto res2 = integrate(a, b, N*2);
+                    auto prec = 4/3 * std::abs(res2.first - res.first) / res.first;
+                    if (prec <= reqPrec) {
+                        return {res.first, prec};
+                    }
+                    N++;
+                }
+            }
+
+            unsigned int GetN() {
+                return N;
+            }
+
+        private:
+            const Func f;
+            const double reqPrec = NAN;
+            unsigned int N = INTEGRAL_MIDPOINT_DEFAULT_NSTEP;
+
+            Result integrate(double a, double b) {
                 auto res = 0.0;
                 const auto h = (b - a) / double(N);
-                for (unsigned int n = 0; n < N - 1; n++) {
-                    const auto xM = (a + n * h + (n + 1) * h) / 2;
+                for (unsigned int n = 0; n < N; n++) {
+                    const auto xM = a + (n + 0.5) * h;
                     res += f(xM);
                 }
                 res *= h;
                 return {res, h * h};
             }
 
-        private:
-            const Func f;
-            const unsigned int N = INTEGRAL_MIDPOINT_DEFAULT_NSTEP;
+            Result integrate(double a, double b, unsigned int N) {
+                auto res = 0.0;
+                const auto h = (b - a) / double(N);
+                for (unsigned int n = 0; n < N; n++) {
+                    const auto xM = a + (n + 0.5) * h;
+                    res += f(xM);
+                }
+                res *= h;
+                return {res, h * h};
+            }
         };
     }
 }
